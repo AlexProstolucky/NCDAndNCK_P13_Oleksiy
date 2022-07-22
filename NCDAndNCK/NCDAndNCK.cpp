aab1417fdc4f7a802ba0517c15b05ca67fe3a337
admin1 <admin1@ADMIN1-PC36>
2022-07-22 17:42:50 +0300

    Добавьте файлы проекта.

---
+++ NCDAndNCK/NCDAndNCK.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+unsigned int NCD(unsigned int a, unsigned int b)
+{
+    if (a == b)
+        return a;
+    if (a > b)
+        return NCD(a - b, b);
+    return NCD(a, b - a);
+}
+
+unsigned int NCK(unsigned int a, unsigned int b) { return (a * b) / NCD(a, b); }
+
+int main()
+{
+    int a = 50, b = 75;
+    cout << NCD(a, b);
+    cout << endl;
+    cout << NCK(a, b);
+
+    return 0;
+}
